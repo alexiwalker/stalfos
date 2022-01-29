8afe1f813980a84ec45cb413be92fcf6b683f9e2
alexiwalker <alexander.i.walker@outlook.com>
2022-01-29 12:29:33 +1000

    some cleanup

--- src/main.rs
+++ src/main.rs
@@ -14,20 +14,11 @@ fn main() {
     }
 
     let path = args[1].clone();
-    //compile hello_world to that path
 
-    let hello_world = example_programs::jmp_except_catch();
-    let hello_world_asm = assemble(&hello_world);
-    assembler::write_to_file(&hello_world_asm, &path);
-
-    //read all bytes from file
     let mut file = File::open(path).unwrap();
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer).unwrap();
     let program = parse_binary(buffer);
 
-    let vm = stalfos::VM::run_new(program);
-
-    println!("{:?}", vm.alloc_table);
-
+    stalfos::VM::run_new(program);
 }
